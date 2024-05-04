d8ba84b0dccd717ab3720af4d3e9d70103a6cb70
L-U-C-A <lucaalbertodalboni2@gmail.com>
2024-05-05 00:56:38 +0200

    Contacts

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-// GENERALE Agg
+// GENERALE
+console.log("%cWebsite entirely created by luca33573 (Discord)", "letter-spacing:.5px; padding:10px; font-size:20px; font-family:sans-serif; font-weight:900; text-shadow: 2px 3px 5px rgba(0,0,0,0.5); border-bottom:solid 1px #101010;")
+
 // Navigation Menu Icon Change
 let ChangeIcon = function(icon) {
     icon.classList.toggle("fa-xmark");
 }
 
-console.log(document.getElementById("title"));
-document.getElementById("title").innerHTML = navigator.userAgent;
+// Kour windows download
+function WDownload() {
+    window.open("https://play.google.com/store/apps/details?id=com.LEGiONPlatforms.Kour&hl=en_US&gl=US")
+}
